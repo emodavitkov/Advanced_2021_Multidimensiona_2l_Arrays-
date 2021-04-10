0c4b809df671c9b25f2ca3ccde2b2528c2e91896
Emil Hristov <emodavitkov@abv.bg>
2021-04-10 15:24:46 +0300

    go

---
+++ 2X2 Squares in Matrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2X2_Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
