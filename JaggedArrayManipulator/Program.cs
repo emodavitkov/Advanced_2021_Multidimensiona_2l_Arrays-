2fd77de77901f45e444e8158c6c642294abcc98e
Emil Hristov <emodavitkov@abv.bg>
2021-04-12 22:58:04 +0300

    GO

--- JaggedArrayManipulator/Program.cs
+++ JaggedArrayManipulator/Program.cs
@@ -7,7 +7,90 @@ namespace JaggedArrayManipulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rows = int.Parse(Console.ReadLine());
+            double[][] matrix = new double[rows][];
+
+            //origin approach to fill the jagged matrix up
+
+            //for (int row = 0; row < n; row++)
+            //{
+            //    int[] currRow = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            //    jaggedMatrix[row] = new int[currRow.Length];
+
+            //    for (int col = 0; col < currRow.Length; col++)
+            //    {
+            //        jaggedMatrix[row][col] = currRow[col];
+            //    }
+            //}
+
+            //second approach # better one:
+            for (int row = 0; row < rows; row++)
+            {
+                double[] currRow = Console.ReadLine().Split().Select(double.Parse).ToArray();
+                matrix[row] = new double[currRow.Length];
+                for (int col = 0; col < currRow.Length; col++)
+                {
+                    matrix[row] = currRow;
+                }
+            }
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                if (matrix[row].Length == matrix[row + 1].Length)
+                {
+                    for (int col = 0; col < matrix[row].Length; col++)
+                    {
+                        matrix[row][col] *= 2;
+                        matrix[row + 1][col] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < matrix[row].Length; col++)
+                    {
+                        matrix[row][col] /= 2;
+                    }
+                    for (int col = 0; col < matrix[row + 1].Length; col++)
+                    {
+                        matrix[row + 1][col] /= 2;
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] splitedCommand = command.Split();
+                int row = int.Parse(splitedCommand[1]);
+                int col = int.Parse(splitedCommand[2]);
+                int value = int.Parse(splitedCommand[3]);
+
+                if (splitedCommand[0] == "Add")
+                {
+                    if (isValidCoordinates(row, col, matrix))
+                    {
+                        matrix[row][col] += value;
+                    }
+                }
+                else
+                {
+                    if (isValidCoordinates(row, col, matrix))
+                    {
+                        matrix[row][col] -= value;
+                    }
+                }
+                command = Console.ReadLine();
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                Console.WriteLine(string.Join(" ", matrix[row]));
+            }
+        }
+
+        private static bool isValidCoordinates(int row, int col, double[][] matrix)
+        {
+            return row >= 0 && col >= 0 && row < matrix.GetLength(0) && col < matrix[row].Length;
         }
     }
 }
---
+++ KnightGame/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace KnightGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int boardDimension = int.Parse(Console.ReadLine());
+            int[,] board = new int[boardDimension, boardDimension];
+
+            for (int row = 0; row < boardDimension; row++)
+            {
+                string currRow = Console.ReadLine();
+                for (int col = 0; col < boardDimension; col++)
+                {
+                    if (currRow[col] == 'K')
+                    {
+                        board[row, col] = -1;
+                    }
+                }
+            }
+
+            int removeKnights = 0;
+            while (true)
+            {
+                int maxAttacks = 0;
+                int maxAttacksRow = 0;
+                int maxAttacksCol = 0;
+
+                for (int row = 0; row < boardDimension; row++)
+                {
+                    for (int col = 0; col < boardDimension; col++)
+                    {
+                        if (board[row, col] != 0)
+                        {
+                            board[row, col] = AttackKnight(row, col, board);
+                            if (board[row, col] > maxAttacks)
+                            {
+                                maxAttacks = board[row, col];
+                                maxAttacksRow = row;
+                                maxAttacksCol = col;
+                            }
+                        }
+                    }
+                }
+
+                if (maxAttacks != 0)
+                {
+                    board[maxAttacksRow, maxAttacksCol] = 0;
+                    removeKnights++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+            Console.WriteLine(removeKnights);
+        }
+
+        private static int AttackKnight(int row, int col, int[,] board)
+        {
+            int[,] allMoves = new int[8, 2]
+            {
+                { -2, -1 },
+                { -2, 1 },
+                { -1, -2 },
+                { -1, 2 },
+                { 2, -1 },
+                { 2, 1 },
+                { 1, -2 },
+                { 1, 2 },
+            };
+            int count = 0;
+            for (int moveRow = 0; moveRow < allMoves.GetLength(0); moveRow++)
+            {
+                int attackRow = row + allMoves[moveRow, 0];
+                int attackCol = col + allMoves[moveRow, 1];
+
+                if (IsValidCoordinates(attackRow, attackCol, board))
+                {
+                    if (board[attackRow, attackCol] != 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        private static bool IsValidCoordinates(int row, int col, int[,] matrix)
+        {
+            return row >= 0 && col >= 0 && row < matrix.GetLength(0) && col < matrix.GetLength(1);
+        }
+    }
+}
